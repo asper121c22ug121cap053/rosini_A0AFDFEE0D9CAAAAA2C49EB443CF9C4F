d89bb9d449649db68cf760b5ee520ff262695ef0
asper121c22ug121cap053 <144932163+asper121c22ug121cap053@users.noreply.github.com>
2023-10-05 23:17:21 -0700

    Add files via upload

---
+++ main(1).py
@@ -0,0 +1,36 @@
+class bankaccount:
+ def __init__(self,account_number,account_holder_name,initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+ def deposit(self,amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("deposited ${}. new balance: ${}".format(amount,self.__account_balance))
+    else:
+      print("invaild deposit amount.")
+ def withdraw(self,amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      # self.__account_balance = self.__account_balance _ amount
+      print("withdrew ${}. new balance: ${}".format(amount,self.__account_balance))
+    else:
+      print("invaild withdrawal amount or insufficiant balance.")
+ def display_balance(self):
+  print("account balance for {} (account #{}): 4{}".format(
+    self.__account_holder_name, self.__account_number,
+    self.__account_balance))
+#create an instance of the bankaccount class
+account = bankaccount(account_number="123456789",
+                     account_holder_name="hari prabu",
+                     initial_balance=5000.0)
+#test deposit and withdrawal functionality
+account.display_balance()
+account.deposit(500.0)
+account.withdraw(200.0)
+account.withdraw(20000.0)
+account.display_balance()
+
+
+    
+      
